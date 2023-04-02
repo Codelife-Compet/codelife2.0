229a1c562f2215ab561976ce0b2eb196ed6422e8
Henrique de Paula Rodrigues <tec.henriquedepaula@gmail.com>
2023-04-02 10:01:31 -0300

    creating mock functions for authentication flow

---
+++ src/services/auth.ts
@@ -0,0 +1,42 @@
+import { v4 as uuid } from "uuid"
+import { User, UserPermissions } from "@/@types/user"
+interface SignInRequestData {
+  email: string,
+  password: string
+}
+interface SignInResponse {
+  token: string,
+  user: User
+}
+interface RecoverRequestData {
+  token: string
+}
+const delay = (amount = 750) => new Promise(resolve => setTimeout(resolve, amount))
+/* Mock the SignIn route from the backend */
+export async function signInRequest(data: SignInRequestData): Promise<SignInResponse> {
+  await delay()
+  // signIn logic (using fetch or axios...)
+  return {
+    token: uuid(),
+    user: {
+      name: "henrique",
+      email: "tec.henriquedepaula@gmail.com",
+      permission: UserPermissions.regular,
+      avatar_url: "https://github.com/bidwolf.png"
+
+    }
+  }
+}
+/* Mock the route that return the info of a token creation like the user,
+ the timestamp or whatever you want*/
+export async function recoverUserInformation(data: RecoverRequestData): Promise<User> {
+  await delay()
+  // Recover logic (using fetch or axios...)
+  return {
+
+    name: "henrique",
+    email: "tec.henriquedepaula@gmail.com",
+    permission: UserPermissions.regular,
+    avatar_url: "https://github.com/bidwolf.png"
+  }
+}
