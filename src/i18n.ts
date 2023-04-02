ed4064ef836a115cf427ab3c0f6e7be635f7007f
Henrique de Paula Rodrigues <tec.henriquedepaula@gmail.com>
2023-04-01 23:36:39 -0300

    removing unused import

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,12 +1,11 @@
 import i18next from "i18next";
 import { initReactI18next } from "react-i18next";
-import languageDetector from "i18next-browser-languagedetector"
 import en from "../public/locales/en/common.json"
 import pt from "../public/locales/pt/common.json"
 i18next
   .use(initReactI18next)
   .init({
-    lng:"pt",
+    lng: "pt",
     fallbackLng: "pt",
     resources: {
       en: {
@@ -16,6 +15,6 @@ i18next
         translation: pt
       }
     },
-    debug: true,
+    debug: process.env.NODE_ENV == "development",
   })
 export default i18next
