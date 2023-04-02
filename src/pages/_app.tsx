5a6f430bce3080dc65742c3fa7dac23c12bb7dcc
Henrique de Paula Rodrigues <tec.henriquedepaula@gmail.com>
2023-04-01 21:37:21 -0300

    Using i18n to use translations in the code

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,14 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
-
+import i18n from "@/i18n"
+import { I18nextProvider } from 'react-i18next'
+import { useRouter } from 'next/router'
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const { locale } = useRouter()
+  i18n.changeLanguage(locale)
+  return (
+    <I18nextProvider i18n={i18n}>
+      <Component {...pageProps} />
+    </I18nextProvider>
+  )
 }
