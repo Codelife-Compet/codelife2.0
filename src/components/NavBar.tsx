78d78873618d59e7525df289840cb6dd70fb9bf7
Francisco <franciscoabreu1408@gmail.com>
2023-04-04 21:26:51 -0300

    Implementando os components

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -10,7 +10,7 @@ function NavBar() {
         <nav className="box-border m-0 flex justify-between items-center gap-8 py-0 px-4 h-20 bg-codelife-green-500">
             <LogoIcon size= {120} /> 
             <ul className="p-0 m-0 list-none flex gap-8 text-sm ">
-                <EnterButton size={20} />
+               <li  className=" text-codelife-green-400 hover:text-codelife-black-900 font-semibold"><EnterButton size={20} /></li>
                 <li className=" text-codelife-green-400 hover:text-codelife-black-900 font-semibold"><a href="/">CRIE UMA CONTA</a></li>
                 <li className=" text-codelife-green-400 hover:text-codelife-black-900 font-semibold"><a href="/">SOBRE O CODELIFE</a></li>
                 <li className=" text-codelife-green-400 hover:text-codelife-black-900 font-semibold"><a href="/">PORTUGUÊS</a></li>
